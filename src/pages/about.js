a38d4bb8b31ab65a940610af0598d07394bf3865
Imri Naim <imri294@gmail.com>
2019-03-02 18:54:07 +0200

    first test - .idea/

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const About = () => (
+  <div>
+    <h1>About Us</h1>
+    <p>
+      asdasdasdasd dasdadasdasd asdasdsad asdasdasd asdasdasd asda dasd as ad
+      asdsadasdasd asdasdasdasd asdasdasd
+    </p>
+  </div>
+)
+
+export default About
